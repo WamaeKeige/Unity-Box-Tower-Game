238e3e45457b895a0d57f3a63c6170ceca08187a
wamaekeige <mkeige@gmail.com>
2021-08-20 12:42:11 +0300

    box tower

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    //Spawn Boxes to spawn multiple boxes.
+    public GameObject box_Prefab;
+
+    public void Start(){
+     
+    }
+    public void SpawnBox(){
+        GameObject box_Obj = Instantiate(box_Prefab);
+        Vector3  temp = transform.position;
+        temp.z = 0f;
+        box_Obj.transform.position = temp;
+    }
+
+    
+    
+    
+    
+}
